418604f0fee2e72197a04b91c70b1d053af60542
hxqmicro <hxqmicro@gmail.com>
2024-03-12 11:58:14 +0800

    [example] add add trait demo

---
+++ examples/add_trait_demo.rs
@@ -0,0 +1,37 @@
+use std::ops::Add;
+
+#[derive(Debug)]
+struct Complex<T> {
+    re: T,
+    im: T,
+}
+
+impl<T> Complex<T> {
+    fn new(re: T, im: T) -> Self
+        where T: Add<T, Output=T>,
+    {
+        Complex { re, im }
+    }
+}
+
+impl<T> Add for Complex<T>
+    where T: Add<T, Output=T>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Complex<T>) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im,
+        }
+    }
+}
+
+
+fn main() {
+    let c1 = Complex::new(1, 2);
+    let c2 = Complex::new(3, 4);
+    let c3 = c1 + c2;
+    println!("{:?}", c3);
+    assert!(c3.re == 4 && c3.im == 6);
+}
